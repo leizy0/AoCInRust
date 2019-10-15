27f2ae6d2c17ce1fdce3765ece94774ba0428fb1
leizy <leizy_post@qq.com>
2019-10-15 16:01:51 +0800

    Solve the second part of day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,24 +1,33 @@
+use std::collections::HashSet;
 use std::fs::File;
-use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::io::{BufRead, BufReader};
 use std::str::FromStr;
 
 fn main() {
     let input_path = "./input.txt";
     let mut freq = 0i32;
+    let mut freq_set = HashSet::new();
 
     let file = File::open(input_path).expect(&format!("Failed to open input file({})", input_path));
-    for line_res in BufReader::new(file).lines() {
-        let change = match line_res {
-            Ok(s) => i32::from_str(s.as_str()).expect(&format!("Failed to parse line({})", s)),
-            Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
-            Err(e) => {
-                writeln!(std::io::stderr(), "Failed to process line, get error({})", e).unwrap();
-                std::process::exit(1)
-            }
-        };
+    let chg_list: Vec<i32> = BufReader::new(file)
+        .lines()
+        .map(|l| {
+            let s = l.unwrap();
+            i32::from_str(s.as_str())
+                .expect(&format!("Failed to parse line({})", &s))
+        })
+        .collect();
 
-        freq += change;
+    let chg_count = chg_list.len();
+    let mut ind = 0;
+    loop {
+        freq_set.insert(freq);
+        freq += chg_list[ind % chg_count];
+        if freq_set.contains(&freq) {
+            println!("Repeat frequency is {}, after {} changes", freq, ind);
+            break;
+        }
+
+        ind += 1;
     }
-
-    println!("The final frequency is {}", freq);
 }
