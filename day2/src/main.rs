9d3f1e02ed222cdde5dc6dddd9d73c05a6ad278c
leizy <leizy_post@qq.com>
2019-10-16 15:58:13 +0800

    Solve the second part of day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,54 +1,54 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::iter::FromIterator;
 
 fn main() {
     let input_path = "./input.txt";
-    let input =
-        File::open(input_path).expect(&format!("Failed to open input file({})", input_path));
+    let input = File::open(input_path).expect(
+        &format!("Failed to open input file({})", input_path));
     let id_list: Vec<String> = BufReader::new(input).lines().map(|l| l.unwrap()).collect();
 
-    let mut rep2_count = 0;
-    let mut rep3_count = 0;
-    for id in id_list {
-        let counter = IdCounter::new(&id);
-        rep2_count += if counter.has_rep_count(2) { 1 } else { 0 };
-        rep3_count += if counter.has_rep_count(3) { 1 } else { 0 };
-    }
+    let id_count = id_list.len();
 
-    let checksum = rep2_count * rep3_count;
-    println!("There are {} box with letter repeats exactly twice, {} box with letter repeats exactly three times, checksum is {}", rep2_count, rep3_count, checksum);
-}
+    // Loop on all pairs of ids
+    for i in 0..id_count {
+        for j in (i + 1)..id_count {
+            let id1 = &id_list[i];
+            let id2 = &id_list[j];
 
-struct IdCounter {
-    id: String,
-    map: HashMap<char, u32>, // Key is char in id, value is repeat count
-}
+            let char_list1: Vec<char> = id1.chars().collect();
+            let char_list2: Vec<char> = id2.chars().collect();
+            if char_list1.len() != char_list2.len() {
+                println!("Two ids({}, {}) have different char count, ignore", id1, id2);
+                continue;
+            }
 
-impl IdCounter {
-    fn new(id: &str) -> IdCounter {
-        let mut map = HashMap::new();
-
-        // Compute repeat count of every letter in id, store it in map
-        for c in id.chars() {
-            let count = map.entry(c).or_insert(0);
-            *count += 1;
-        }
-
-        IdCounter {
-            id: id.to_string(),
-            map,
-        }
-    }
-
-    // Is there any chars in id have given repeat count?
-    fn has_rep_count(&self, count: u32) -> bool {
-        for (_k, v) in &self.map {
-            if *v == count {
-                return true;
+            let com_chars = list_com_elems(&char_list1, &char_list2);
+            if com_chars.len() == (char_list1.len() - 1) {
+                println!("Common sequence({}) with only one char different found, between {} and {}",
+                    String::from_iter(com_chars.iter()), id1, id2);
+                return;
             }
         }
-
-        false
     }
+    
+    println!("Can't find any id pair has exactly one char different");
+    std::process::exit(1);
+}
+
+// List all same elements at same position between given vectors
+// if two vector have different length, then return empty result
+fn list_com_elems<T: std::cmp::PartialEq + Copy>(v1: &Vec<T>, v2: &Vec<T>) -> Vec<T> {
+    let mut com_elems = Vec::new();
+    if v1.len() != v2.len() {
+        return com_elems;
+    }
+
+    for i in 0..(v1.len()) {
+        if v1[i] == v2[i] {
+            com_elems.push(v1[i]);
+        }
+    }
+
+    com_elems
 }
