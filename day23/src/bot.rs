5992b92204a2976cf0d26aa69557ac6e5511bdec
Leizy <leizy0@users.noreply.github.com>
2022-08-12 14:53:38 +0800

    Solved the second part of day 23. Two ideas are important, one is using elimination to get the largest node set in which each one is neighbor with others; another is putting a circle at origin to compute distance. They all use binary search though have different contexts

--- day23/src/bot.rs
+++ day23/src/bot.rs
@@ -1,11 +1,17 @@
-use std::{path::Path, fs::File, io::{BufReader, BufRead}, fmt::Display};
+use std::{
+    fmt::Display,
+    fs::File,
+    io::{BufRead, BufReader},
+    path::Path,
+};
 
 use once_cell::sync::Lazy;
 use regex::Regex;
 
 use crate::Error;
 
-struct Position {
+#[derive(Debug, Clone, Copy)]
+pub struct Position {
     x: isize,
     y: isize,
     z: isize,
@@ -24,11 +30,10 @@ impl Position {
 
     pub fn mht_dist(&self, other: &Self) -> usize {
         (self.x - other.x).unsigned_abs()
-        + (self.y - other.y).unsigned_abs()
-        + (self.z - other.z).unsigned_abs()
+            + (self.y - other.y).unsigned_abs()
+            + (self.z - other.z).unsigned_abs()
     }
 }
-
 
 pub struct Nanobot {
     pos: Position,
@@ -39,13 +44,16 @@ impl TryFrom<&str> for Nanobot {
     type Error = Error;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        static NANOBOT_PATTERN: Lazy<Regex> = Lazy::new(|| Regex::new(r"pos=<(-?\d+),(-?\d+),(-?\d+)>,\s+r=(\d+)").unwrap());
-        let caps = NANOBOT_PATTERN.captures(value).ok_or(Error::NotMatchNanobotPattern(value.to_string()))?;
+        static NANOBOT_PATTERN: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"pos=<(-?\d+),(-?\d+),(-?\d+)>,\s+r=(\d+)").unwrap());
+        let caps = NANOBOT_PATTERN
+            .captures(value)
+            .ok_or(Error::NotMatchNanobotPattern(value.to_string()))?;
         Ok(Nanobot {
             pos: Position::new(
                 caps[1].parse::<isize>().unwrap(),
                 caps[2].parse::<isize>().unwrap(),
-                caps[3].parse::<isize>().unwrap()
+                caps[3].parse::<isize>().unwrap(),
             ),
             signal_radius: caps[4].parse::<usize>().unwrap(),
         })
@@ -54,24 +62,50 @@ impl TryFrom<&str> for Nanobot {
 
 impl Display for Nanobot {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "(pos: {}, signal radius: {})", self.pos, self.signal_radius)
+        write!(
+            f,
+            "(pos: {}, signal radius: {})",
+            self.pos, self.signal_radius
+        )
     }
 }
 
 impl Nanobot {
+    pub fn new(pos: &Position, signal_radius: usize) -> Nanobot {
+        Nanobot {
+            pos: *pos,
+            signal_radius,
+        }
+    }
+
     pub fn signal_rad(&self) -> usize {
         self.signal_radius
+    }
+
+    pub fn pos(&self) -> Position {
+        self.pos
     }
 
     pub fn is_in_range(&self, other: &Self) -> bool {
         self.pos.mht_dist(&other.pos) <= self.signal_radius
     }
+
+    pub fn has_intersection(&self, other: &Self) -> bool {
+        self.pos.mht_dist(&other.pos) <= self.signal_radius + other.signal_radius
+    }
 }
 
-pub fn load_bots<P>(input_path: P) -> Result<Vec<Nanobot>, Error> where P: AsRef<Path> {
+pub fn load_bots<P>(input_path: P) -> Result<Vec<Nanobot>, Error>
+where
+    P: AsRef<Path>,
+{
     let input_file = File::open(input_path).map_err(Error::IOError)?;
     let reader = BufReader::new(input_file);
-    reader.lines()
-        .map(|lr| lr.map_err(Error::IOError).and_then(|l| Nanobot::try_from(l.as_str())))
+    reader
+        .lines()
+        .map(|lr| {
+            lr.map_err(Error::IOError)
+                .and_then(|l| Nanobot::try_from(l.as_str()))
+        })
         .collect::<Result<Vec<_>, Error>>()
 }
