4ba91377dbf5d6c9c25f28a805d21c7a72484306
Leizy <leizy0@users.noreply.github.com>
2023-07-27 16:53:44 +0800

    Solve part 2 of day 9 in 2019

---
+++ 2019/day2_5_7_9/src/bin/day9_part2.rs
@@ -0,0 +1,29 @@
+use day2_5_7_9::int_code::{com::IntCodeComputer, read_int_code};
+
+fn main() {
+    let input_path = "day9_inputs.txt";
+    let int_code = match read_int_code(input_path) {
+        Ok(ic) => ic,
+        Err(e) => {
+            eprintln!(
+                "Failed to read int code from file({}), get error({})",
+                input_path, e
+            );
+            return;
+        }
+    };
+
+    let mut computer = IntCodeComputer::new(true);
+    let test_mode_input = vec![2];
+    match computer.execute(int_code, test_mode_input) {
+        Ok(res) => println!(
+            "Boost program in sensor boost mode takes {} steps to finish, get outputs({:?})",
+            res.step_count(),
+            res.outputs()
+        ),
+        Err(e) => eprintln!(
+            "Failed to execute Boost program in sensor boost, get error({})",
+            e
+        ),
+    }
+}
