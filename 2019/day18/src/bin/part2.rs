4dd3e7780691e8deba61ef1bd19a12ba26337e67
Leizy <leizy0@users.noreply.github.com>
2024-04-08 15:49:44 +0800

    Solved part 2 of day 18 in 2019, used the same algorithms, but more positions should be considered in process, the node of graph(state of all collectors) also changed.

--- 2019/day18/src/bin/part2.rs
+++ 2019/day18/src/bin/part2.rs
@@ -2,12 +2,15 @@ use day18::Error;
 
 fn main() -> Result<(), Error> {
     let input_path = "inputs.txt";
-    let vault_map = day18::read_vault_map(input_path)?;
+    let mut vault_map = day18::read_vault_map(input_path)?;
+
+    // Change map to match requirements in part 2.
+    day18::split_entrance(&mut vault_map);
     let shortest_collect_path = day18::find_shortest_collect_path(&vault_map);
     let shortest_collect_path_steps_n: usize =
         shortest_collect_path.iter().map(|path| path.len()).sum();
     println!(
-        "The shortest path in given vault to collect keys with {} collector(s) has {} steps.",
+        "The shortest path in given vault(after entrance splited) to collect keys with {} collector(s) has {} steps.",
         vault_map.entrance_n(),
         shortest_collect_path_steps_n
     );
--- 2019/day18/src/lib.rs
+++ 2019/day18/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
     cell::RefCell,
     cmp::Reverse,
-    collections::{BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList},
+    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList},
     fmt::Display,
     fs::File,
     io::{self, BufRead, BufReader},
@@ -69,7 +69,7 @@ impl Direction {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct Position(usize, usize);
 
 impl Display for Position {
@@ -106,7 +106,7 @@ pub struct VaultMap {
     map: Vec<TileType>,
     row_n: usize,
     col_n: usize,
-    entrance_ind: usize,
+    entrance_ind: Vec<usize>,
 }
 
 impl VaultMap {
@@ -143,12 +143,19 @@ impl VaultMap {
             map,
             row_n,
             col_n: col_n.unwrap_or(0),
-            entrance_ind: entrance_ind.unwrap(),
+            entrance_ind: vec![entrance_ind.unwrap()],
         })
     }
 
-    pub fn entrance_pos(&self) -> Position {
-        self.ind_to_pos(self.entrance_ind)
+    pub fn entrance_n(&self) -> usize {
+        self.entrance_ind.len()
+    }
+
+    pub fn entrance_pos(&self) -> Vec<Position> {
+        self.entrance_ind
+            .iter()
+            .map(|ind| self.ind_to_pos(*ind))
+            .collect()
     }
 
     pub fn can_pass(&self, pos: &Position) -> bool {
@@ -157,6 +164,21 @@ impl VaultMap {
 
     pub fn tile(&self, pos: &Position) -> &TileType {
         &self.map[self.pos_to_ind(pos)]
+    }
+
+    pub fn tile_mut(&mut self, pos: &Position) -> &mut TileType {
+        let ind = self.pos_to_ind(pos);
+        &mut self.map[ind]
+    }
+
+    pub fn clear_entrance(&mut self) {
+        self.entrance_ind.clear();
+    }
+
+    pub fn add_entrance(&mut self, pos: &Position) {
+        assert!(*self.tile(pos) != TileType::Wall);
+        let ind = self.pos_to_ind(pos);
+        self.entrance_ind.push(ind);
     }
 
     fn ind_to_pos(&self, ind: usize) -> Position {
@@ -173,130 +195,144 @@ impl VaultMap {
 type PathsToKeys = HashMap<char, (BTreeSet<char>, Vec<Direction>)>;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct CollectorState {
-    pos: Position,
+struct CollectorTeamState {
+    pos: BTreeMap<Position, usize>,
     hold_keys: BTreeSet<char>,
 }
 
 #[derive(Debug, Clone)]
-struct Collector {
-    state: CollectorState,
-    path: Vec<Direction>,
+struct CollectorTeam {
+    members_pos: Vec<Position>,
+    hold_keys: BTreeSet<char>,
+    members_path: Vec<Vec<Direction>>,
     global_paths_to_keys: Rc<RefCell<HashMap<Position, PathsToKeys>>>,
 }
 
-impl PartialEq for Collector {
+impl PartialEq for CollectorTeam {
     fn eq(&self, other: &Self) -> bool {
         self.steps_n() == other.steps_n()
     }
 }
 
-impl Eq for Collector {}
+impl Eq for CollectorTeam {}
 
-impl PartialOrd for Collector {
+impl PartialOrd for CollectorTeam {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         self.steps_n().partial_cmp(&other.steps_n())
     }
 }
 
-impl Ord for Collector {
+impl Ord for CollectorTeam {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.steps_n().cmp(&other.steps_n())
     }
 }
 
-impl Display for Collector {
+impl Display for CollectorTeam {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "collector(at {}, has {:?}, {} steps)",
-            self.state.pos,
-            self.state.hold_keys,
+            "collector(at {:?}, has {:?}, {} steps)",
+            self.members_pos,
+            self.hold_keys,
             self.steps_n()
         )
     }
 }
 
-impl Collector {
-    pub fn new(pos: &Position) -> Self {
+impl CollectorTeam {
+    pub fn new(pos: &[Position]) -> Self {
         Self {
-            state: CollectorState {
-                pos: *pos,
-                hold_keys: BTreeSet::new(),
-            },
-            path: Vec::new(),
+            members_pos: Vec::from(pos),
+            hold_keys: BTreeSet::new(),
+            members_path: vec![Vec::new(); pos.len()],
             global_paths_to_keys: Rc::new(RefCell::new(HashMap::new())),
         }
     }
 
-    pub fn search_keys(&self, map: &VaultMap) -> LinkedList<char> {
-        LinkedList::from_iter(
-            self.global_paths_to_keys
-                .borrow_mut()
-                .entry(self.state.pos)
-                // If paths from current position are not cached, do search and save.
-                .or_insert_with(|| self.bfs_for_keys(map))
-                .iter()
-                .filter(|(key, (keys_needed, _))| {
-                    // Keys are not hold now, and are reachable with current state(from current position, hold current keys).
-                    !self.state.hold_keys.contains(key)
-                        && self.state.hold_keys.is_superset(keys_needed)
-                })
-                .map(|(key, _)| *key),
-        )
+    pub fn search_keys(&self, map: &VaultMap) -> Vec<LinkedList<char>> {
+        self.members_pos
+            .iter()
+            .map(|pos| {
+                LinkedList::from_iter(
+                    self.global_paths_to_keys
+                        .borrow_mut()
+                        .entry(*pos)
+                        // If paths from current position are not cached, do search and save.
+                        .or_insert_with(|| Self::bfs_for_keys(pos, map))
+                        .iter()
+                        .filter(|(key, (keys_needed, _))| {
+                            // Keys are not hold now, and are reachable with current state(from current position, hold current keys).
+                            !self.hold_keys.contains(key) && self.hold_keys.is_superset(keys_needed)
+                        })
+                        .map(|(key, _)| *key),
+                )
+            })
+            .collect()
     }
 
     pub fn steps_n(&self) -> usize {
-        self.path.len()
+        self.members_path.iter().map(|p| p.len()).sum()
     }
 
-    pub fn path(&self) -> &Vec<Direction> {
-        &self.path
+    pub fn path(&self) -> &Vec<Vec<Direction>> {
+        &self.members_path
     }
 
-    pub fn state(&self) -> &CollectorState {
-        &self.state
+    pub fn state(&self) -> CollectorTeamState {
+        let mut pos = BTreeMap::new();
+        for p in &self.members_pos {
+            *pos.entry(*p).or_insert(0) += 1;
+        }
+
+        CollectorTeamState {
+            pos,
+            hold_keys: self.hold_keys.clone(),
+        }
     }
 
-    pub fn move_to_key(&mut self, key: char, map: &VaultMap) {
-        // Assume the shortest path to this key from current position has been found in earlier search(bfs_for_keys).
+    pub fn move_mem_to_key(&mut self, member_ind: usize, key: char, map: &VaultMap) {
+        assert!(member_ind < self.members_pos.len());
+        // Assume the shortest path to this key from gien member's position has been found in earlier search(bfs_for_keys).
         if let Some((keys_needed, path)) = self
             .global_paths_to_keys
             .borrow()
-            .get(&self.state.pos)
+            .get(&self.members_pos[member_ind])
             .and_then(|paths| paths.get(&key))
         {
-            if !self.state.hold_keys.is_superset(keys_needed) {
-                panic!("Collector(at {}, have {:?}) doesn't have enough keys({:?}) to reach given key({})", self.state.pos, self.state.hold_keys, keys_needed, key);
+            if !self.hold_keys.is_superset(keys_needed) {
+                panic!("Collector member(at {}, have {:?}) doesn't have enough keys({:?}) to reach given key({})", self.members_pos[member_ind], self.hold_keys, keys_needed, key);
             }
 
-            // Add the shortest path to given key to self path
+            // Add the shortest path to given key to member's path
+            let member_pos = &mut self.members_pos[member_ind];
+            let member_path = &mut self.members_path[member_ind];
             for dir in path {
-                self.state.pos = self.state.pos.move_along(*dir).unwrap();
-                assert!(map.can_pass(&self.state.pos));
-                self.path.push(*dir);
+                *member_pos = member_pos.move_along(*dir).unwrap();
+                assert!(map.can_pass(member_pos));
+                member_path.push(*dir);
             }
-            assert!(*map.tile(&self.state.pos) == TileType::Key(key));
+            assert!(*map.tile(&member_pos) == TileType::Key(key));
             // Hold the key when reach its position.
-            self.state.hold_keys.insert(key);
+            self.hold_keys.insert(key);
         } else {
             panic!(
                 "Given key({}) isn't reachable to this collector(at {}, have {:?})",
-                key, self.state.pos, self.state.hold_keys
+                key, self.members_pos[member_ind], self.hold_keys
             );
         }
     }
 
-    fn bfs_for_keys(&self, map: &VaultMap) -> PathsToKeys {
-        let init_pos = self.state.pos;
+    fn bfs_for_keys(pos: &Position, map: &VaultMap) -> PathsToKeys {
+        let init_pos = *pos;
         let mut positions = LinkedList::new();
         positions.push_back(init_pos);
         let mut paths = HashMap::new();
         let mut paths_to_keys = HashMap::new();
 
-        // Breadth first search for shortest path to all reachable position from current position.
+        // Breadth first search for shortest path to all reachable position from given position.
         while let Some(pos) = positions.pop_front() {
-            // Positions can move to from current position, wasn't reached before, and can pass(not a barrier).
+            // Positions can move to from current position, wasn't reached before, and collector can pass it(not a barrier).
             let move_positions = [
                 Direction::North,
                 Direction::South,
@@ -350,31 +386,65 @@ pub fn read_vault_map<P: AsRef<Path>>(path: P) -> Result<VaultMap, Error> {
     VaultMap::try_from_lines(map_lines.iter().map(|s| s.as_str()))
 }
 
-pub fn find_shortest_collect_path(map: &VaultMap) -> Vec<Direction> {
-    let init_collector = Collector::new(&map.entrance_pos());
-    let mut collectors = BinaryHeap::new();
-    let mut searched_collector_states = HashSet::new();
-    collectors.push(Reverse(init_collector));
+pub fn split_entrance(map: &mut VaultMap) {
+    let entrances = map.entrance_pos();
+    assert!(entrances.len() == 1);
 
-    // Dijkstra's search for the shortest path to collect all keys. The states of collectors are nodes in graph.
-    while let Some(Reverse(collector)) = collectors.pop() {
-        if searched_collector_states.contains(collector.state()) {
-            // Ignore collectors with state has been searched
+    // Change position of entrance to wall.
+    let entrance_pos = entrances[0];
+    *map.tile_mut(&entrance_pos) = TileType::Wall;
+    // Change four neighbors of entrance to wall too.
+    for pos in [
+        Direction::North,
+        Direction::South,
+        Direction::West,
+        Direction::East,
+    ]
+    .iter()
+    .flat_map(|dir| entrance_pos.move_along(*dir))
+    {
+        *map.tile_mut(&pos) = TileType::Wall;
+    }
+
+    // Replace the entrance to new entrances at four diagonal positions of the original one.
+    map.clear_entrance();
+    map.add_entrance(&Position::new(entrance_pos.x() - 1, entrance_pos.y() - 1));
+    map.add_entrance(&Position::new(entrance_pos.x() - 1, entrance_pos.y() + 1));
+    map.add_entrance(&Position::new(entrance_pos.x() + 1, entrance_pos.y() - 1));
+    map.add_entrance(&Position::new(entrance_pos.x() + 1, entrance_pos.y() + 1));
+}
+
+pub fn find_shortest_collect_path(map: &VaultMap) -> Vec<Vec<Direction>> {
+    let init_team = CollectorTeam::new(&map.entrance_pos());
+    let mut collector_teams = BinaryHeap::new();
+    let mut searched_team_states = HashSet::new();
+    collector_teams.push(Reverse(init_team));
+
+    // Dijkstra's search for the shortest path to collect all keys. The states of collector teams are nodes in graph.
+    while let Some(Reverse(team)) = collector_teams.pop() {
+        let state = team.state();
+        if searched_team_states.contains(&state) {
+            // Ignore collector teams with state has been searched
             continue;
         }
 
-        searched_collector_states.insert(collector.state().clone());
-        let reachable_keys = collector.search_keys(map);
-        if reachable_keys.is_empty() {
+        searched_team_states.insert(state);
+        let team_reachable_keys = team.search_keys(map);
+        if team_reachable_keys
+            .iter()
+            .all(|member_reachable_keys| member_reachable_keys.is_empty())
+        {
             // The first completed path is one of the shortest path.
-            return collector.path().clone();
+            return team.path().clone();
         }
 
-        for key in reachable_keys {
-            // Move one cloned collector to each reachable keys, and record them for future search.
-            let mut future_collector = collector.clone();
-            future_collector.move_to_key(key, map);
-            collectors.push(Reverse(future_collector));
+        for (member_ind, member_reachable_keys) in team_reachable_keys.iter().enumerate() {
+            for key in member_reachable_keys {
+                // Move one cloned collector team to each reachable keys, and record them for future search.
+                let mut future_team = team.clone();
+                future_team.move_mem_to_key(member_ind, *key, map);
+                collector_teams.push(Reverse(future_team));
+            }
         }
     }
 
