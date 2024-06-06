900bec4e61f7b19dece5f675a52ad30b94b1b7c9
Leizy <leizy0@users.noreply.github.com>
2024-06-06 22:42:55 +0800

    Solved part 1 of day 7 in 2020, basic BFS in directed graph.

---
+++ 2020/day7/src/bin/part1.rs
@@ -0,0 +1,24 @@
+use clap::Parser;
+use day7::CliArgs;
+
+fn main() {
+    let args = CliArgs::parse();
+    let bag_rules = day7::read_br(&args.input_path)
+        .inspect_err(|e| {
+            eprintln!(
+                "Failed to read bag rules from given input file({}), get error({}).",
+                args.input_path.display(),
+                e
+            )
+        })
+        .unwrap();
+    let bag_qualifier = "shiny gold";
+    if let Some(contain_n) = bag_rules.contained_kinds_n(bag_qualifier) {
+        println!(
+            "There are {} kinds of bag can contain at least one specified {} bag.",
+            contain_n, bag_qualifier
+        );
+    } else {
+        println!("There's no {} bag in given rules.", bag_qualifier);
+    }
+}
