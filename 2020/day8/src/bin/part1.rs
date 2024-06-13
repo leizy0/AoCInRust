7e1bdf7d233f951e80d3b773f67e3bab5d584730
Leizy <leizy0@users.noreply.github.com>
2024-06-13 11:41:29 +0800

    Solved part 1 of day 8 in 2020, yet another intcode?

---
+++ 2020/day8/src/bin/part1.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+use clap::Parser;
+use day8::{CliArgs, GCState, GameConsle};
+
+fn main() {
+    let args = CliArgs::parse();
+    let code = day8::read_code(&args.input_path)
+        .inspect_err(|e| {
+            eprintln!(
+                "Failed to read code from given input file({}), get error({}).",
+                args.input_path.display(),
+                e
+            )
+        })
+        .unwrap();
+    let mut console = GameConsle::new();
+    let mut inst_ind_set = HashSet::new();
+    match console.run_while(&code, &mut |state: &GCState| inst_ind_set.insert(state.inst_ptr)) {
+        Ok(state) => println!("Before the first repeated execution of a same instruction(at {}), the accumulator is {}.", state.inst_ptr, state.acc),
+        Err(e) => eprint!("Failed to run given code, get error({}).", e),
+    }
+}
