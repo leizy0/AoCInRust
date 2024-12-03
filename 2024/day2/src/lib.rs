52790c3ab28f09abe4ef1c3c1fee1d3c89c09a13
Leizy <leizy0@users.noreply.github.com>
2024-12-03 15:28:16 +0800

    Solved part 2 of day 2 in 2024, in total three locations should be considered to remove, and the first is easy to be ignored.

--- 2024/day2/src/lib.rs
+++ 2024/day2/src/lib.rs
@@ -65,28 +65,80 @@ impl TryFrom<&str> for Report {
 
 impl Report {
     pub fn is_safe(&self) -> bool {
-        let level_n = self.levels.len();
-        if level_n <= 1 {
+        let (len, _) = Self::safe_len(&self.levels, None);
+        len == self.levels.len()
+    }
+
+    pub fn is_tolerantly_safe(&self) -> bool {
+        fn is_safe_if_remove(levels: &[usize], ind: usize, last_is_inc_req: Option<bool>) -> bool {
+            if ind == levels.len() - 1 {
+                return true;
+            }
+
+            let is_inc_req = if ind == 0 {
+                None
+            } else {
+                let last_is_inc_req = if ind == 1 { None } else { last_is_inc_req };
+
+                let (is_safe, is_inc) =
+                    Report::is_safe_pair(levels[ind - 1], levels[ind + 1], last_is_inc_req);
+                if !is_safe {
+                    return false;
+                }
+
+                Some(is_inc)
+            };
+            let left_levels = &levels[(ind + 1)..];
+            let (len, _) = Report::safe_len(left_levels, is_inc_req);
+
+            len == left_levels.len()
+        }
+
+        let (len, last_is_inc_op) = Self::safe_len(&self.levels, None);
+        if len == self.levels.len() {
             return true;
         }
 
-        let mut is_inc_op = None;
-        for (last_l, cur_l) in self.levels[..(level_n - 1)]
-            .iter()
-            .zip(self.levels[1..].iter())
-        {
-            let cur_is_inc = last_l < cur_l;
-            if *is_inc_op.get_or_insert(cur_is_inc) == cur_is_inc {
-                let abs_diff = last_l.abs_diff(*cur_l);
-                if abs_diff >= 1 && abs_diff <= 3 {
-                    continue;
-                }
-            }
-
-            return false;
+        // Remove the first level.
+        if len == 2 && is_safe_if_remove(&self.levels, 0, last_is_inc_op) {
+            return true;
         }
 
-        true
+        // Remove the left level.
+        is_safe_if_remove(&self.levels, len - 1, last_is_inc_op) ||
+        // Remove the right level.
+            is_safe_if_remove(&self.levels, len, last_is_inc_op)
+    }
+
+    fn safe_len(levels: &[usize], is_inc_req: Option<bool>) -> (usize, Option<bool>) {
+        let level_n = levels.len();
+        if level_n <= 1 {
+            return (level_n, None);
+        }
+
+        let mut is_inc_op = is_inc_req;
+        let mut pair_n = 0;
+        for (last_l, cur_l) in levels[..(level_n - 1)].iter().zip(levels[1..].iter()) {
+            let (is_safe, cur_is_inc) = Self::is_safe_pair(*last_l, *cur_l, is_inc_op);
+            is_inc_op.get_or_insert(cur_is_inc);
+            if !is_safe {
+                break;
+            }
+
+            pair_n += 1;
+        }
+
+        (pair_n + 1, is_inc_op)
+    }
+
+    fn is_safe_pair(l_level: usize, r_level: usize, is_inc_req: Option<bool>) -> (bool, bool) {
+        let cur_is_inc = l_level < r_level;
+        if is_inc_req.is_some_and(|is_inc| is_inc != cur_is_inc) {
+            return (false, cur_is_inc);
+        }
+
+        let abs_diff = l_level.abs_diff(r_level);
+        (abs_diff >= 1 && abs_diff <= 3, cur_is_inc)
     }
 }
 
