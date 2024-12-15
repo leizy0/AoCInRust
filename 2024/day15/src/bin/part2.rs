92b1f5f35f6ca691c04461299d7150ac424001e2
Leizy <leizy0@users.noreply.github.com>
2024-12-15 17:56:35 +0800

    Solved part 2 of day 15 in 2024, a bigger box brings to a more difficult collison detection, the all paths that can transmit force should be checked before the final actual move.

--- 2024/day15/src/bin/part2.rs
+++ 2024/day15/src/bin/part2.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use clap::Parser;
-use day15::{BoxGame, CLIArgs, PlainTile};
+use day15::{CLIArgs, WideBoxGame, WideTile};
 
 fn main() -> Result<()> {
     let args = CLIArgs::parse();
@@ -11,15 +11,16 @@ fn main() -> Result<()> {
         )
     })?;
 
-    let mut game = BoxGame::new(map);
+    let wide_map = map.widen();
+    let mut game = WideBoxGame::new(wide_map);
     game.simulate(&move_dirs);
     let coord_sum = game
         .map()
-        .position_iter(PlainTile::Box)
+        .position_iter(WideTile::WideBoxLeft)
         .map(|p| p.r * 100 + p.c)
         .sum::<usize>();
     println!(
-        "After move(s), he sum of boxes' GPS coordinates in map is {}.",
+        "After move(s), the sum of boxes' GPS coordinates in widened map is {}.",
         coord_sum
     );
 
--- 2024/day15/src/lib.rs
+++ 2024/day15/src/lib.rs
@@ -47,13 +47,55 @@ pub struct CLIArgs {
     pub input_path: PathBuf,
 }
 
+pub trait Tile: Copy + Eq + Display {}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Tile {
+pub enum PlainTile {
     Wall,
     Robot,
     Box,
     Floor,
 }
+
+impl Display for PlainTile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let tile_char = match self {
+            PlainTile::Wall => '#',
+            PlainTile::Robot => '@',
+            PlainTile::Box => 'O',
+            PlainTile::Floor => '.',
+        };
+
+        write!(f, "{}", tile_char)
+    }
+}
+
+impl Tile for PlainTile {}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum WideTile {
+    Wall,
+    WideBoxLeft,
+    WideBoxRight,
+    Robot,
+    Floor,
+}
+
+impl Display for WideTile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let tile_char = match self {
+            WideTile::Wall => '#',
+            WideTile::Robot => '@',
+            WideTile::WideBoxLeft => '[',
+            WideTile::WideBoxRight => ']',
+            WideTile::Floor => '.',
+        };
+
+        write!(f, "{}", tile_char)
+    }
+}
+
+impl Tile for WideTile {}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Position {
@@ -83,7 +125,7 @@ impl Position {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Direction {
     Up,
     Right,
@@ -106,25 +148,19 @@ impl TryFrom<char> for Direction {
 }
 
 #[derive(Debug)]
-pub struct Map {
-    tiles: Vec<Tile>,
+pub struct Map<T: Tile> {
+    tiles: Vec<T>,
     robot_pos: Position,
     row_n: usize,
     col_n: usize,
 }
 
-impl Display for Map {
+impl<T: Tile> Display for Map<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for r in 0..self.row_n {
             for c in 0..self.col_n {
                 let pos = Position::new(r, c);
-                let tile_char = match self.tile(&pos).unwrap() {
-                    Tile::Wall => '#',
-                    Tile::Robot => '@',
-                    Tile::Box => 'O',
-                    Tile::Floor => '.',
-                };
-                write!(f, "{}", tile_char)?;
+                write!(f, "{}", self.tile(&pos).unwrap())?;
             }
             writeln!(f, "")?;
         }
@@ -133,14 +169,8 @@ impl Display for Map {
     }
 }
 
-impl Map {
-    pub fn simulate(&mut self, dirs: &[Direction]) {
-        for dir in dirs {
-            self.try_move(&self.robot_pos.clone(), *dir);
-        }
-    }
-
-    pub fn position_iter(&self, tile: Tile) -> impl Iterator<Item = Position> + use<'_> {
+impl<T: Tile> Map<T> {
+    pub fn position_iter(&self, tile: T) -> impl Iterator<Item = Position> + use<'_, T> {
         self.tiles
             .iter()
             .enumerate()
@@ -148,41 +178,11 @@ impl Map {
             .map(|(ind, _)| self.ind_to_pos(ind).unwrap())
     }
 
-    fn try_move(&mut self, pos: &Position, dir: Direction) -> bool {
-        if let Some(cur_tile) = self.tile(pos).cloned() {
-            match cur_tile {
-                Tile::Robot | Tile::Box => {
-                    if let Some(next_position) = pos.neighbor(dir) {
-                        if let Some(next_tile) = self.tile(&next_position).cloned() {
-                            match next_tile {
-                                Tile::Box | Tile::Floor => {
-                                    if self.try_move(&next_position, dir) {
-                                        *self.tile_mut(&next_position).unwrap() = cur_tile;
-                                        *self.tile_mut(&pos).unwrap() = Tile::Floor;
-                                        if cur_tile == Tile::Robot {
-                                            self.robot_pos = next_position;
-                                        }
-                                        return true;
-                                    }
-                                }
-                                _ => (),
-                            }
-                        }
-                    }
-                }
-                Tile::Floor => return true,
-                _ => (),
-            }
-        }
-
-        false
-    }
-
-    fn tile(&self, pos: &Position) -> Option<&Tile> {
+    fn tile(&self, pos: &Position) -> Option<&T> {
         self.pos_to_ind(pos).and_then(|ind| self.tiles.get(ind))
     }
 
-    fn tile_mut(&mut self, pos: &Position) -> Option<&mut Tile> {
+    fn tile_mut(&mut self, pos: &Position) -> Option<&mut T> {
         self.pos_to_ind(pos).and_then(|ind| self.tiles.get_mut(ind))
     }
 
@@ -203,14 +203,248 @@ impl Map {
     }
 }
 
-pub struct MapBuilder {
-    tiles: Vec<Tile>,
+impl Map<PlainTile> {
+    pub fn widen(&self) -> Map<WideTile> {
+        let mut tiles = Vec::with_capacity(self.tiles.len() * 2);
+        for tile in self.tiles.iter() {
+            tiles.extend_from_slice(&match tile {
+                PlainTile::Wall => [WideTile::Wall, WideTile::Wall],
+                PlainTile::Robot => [WideTile::Robot, WideTile::Floor],
+                PlainTile::Box => [WideTile::WideBoxLeft, WideTile::WideBoxRight],
+                PlainTile::Floor => [WideTile::Floor, WideTile::Floor],
+            });
+        }
+        let robot_pos = Position::new(self.robot_pos.r, self.robot_pos.c * 2);
+
+        Map::<WideTile> {
+            tiles,
+            robot_pos,
+            row_n: self.row_n,
+            col_n: self.col_n * 2,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct BoxGame {
+    map: Map<PlainTile>,
+}
+
+impl BoxGame {
+    pub fn new(map: Map<PlainTile>) -> Self {
+        Self { map }
+    }
+
+    pub fn simulate(&mut self, dirs: &[Direction]) {
+        for dir in dirs {
+            self.try_move(&self.map.robot_pos.clone(), *dir);
+        }
+    }
+
+    fn try_move(&mut self, pos: &Position, dir: Direction) -> bool {
+        if let Some(cur_tile) = self.map.tile(pos).cloned() {
+            match cur_tile {
+                PlainTile::Robot | PlainTile::Box => {
+                    if let Some(next_position) = pos.neighbor(dir) {
+                        if let Some(next_tile) = self.map.tile(&next_position).cloned() {
+                            match next_tile {
+                                PlainTile::Box | PlainTile::Floor => {
+                                    if self.try_move(&next_position, dir) {
+                                        *self.map.tile_mut(&next_position).unwrap() = cur_tile;
+                                        *self.map.tile_mut(&pos).unwrap() = PlainTile::Floor;
+                                        if cur_tile == PlainTile::Robot {
+                                            self.map.robot_pos = next_position;
+                                        }
+                                        return true;
+                                    }
+                                }
+                                _ => (),
+                            }
+                        }
+                    }
+                }
+                PlainTile::Floor => return true,
+                _ => (),
+            }
+        }
+
+        false
+    }
+
+    pub fn map(&self) -> &Map<PlainTile> {
+        &self.map
+    }
+}
+
+#[derive(Debug)]
+pub struct WideBoxGame {
+    map: Map<WideTile>,
+}
+
+impl WideBoxGame {
+    pub fn new(map: Map<WideTile>) -> Self {
+        Self { map }
+    }
+
+    pub fn simulate(&mut self, dirs: &[Direction]) {
+        for dir in dirs {
+            self.try_move(&self.map.robot_pos.clone(), *dir);
+        }
+    }
+
+    fn try_move(&mut self, pos: &Position, dir: Direction) -> bool {
+        if let Some(cur_tile) = self.map.tile(pos).cloned() {
+            match cur_tile {
+                WideTile::WideBoxLeft => {
+                    return self.try_move_box(pos, &pos.neighbor(Direction::Right).unwrap(), dir)
+                }
+                WideTile::WideBoxRight => {
+                    return self.try_move_box(&pos.neighbor(Direction::Left).unwrap(), pos, dir)
+                }
+                WideTile::Robot => {
+                    if let Some(next_position) = pos.neighbor(dir) {
+                        if let Some(next_tile) = self.map.tile(&next_position).cloned() {
+                            if match next_tile {
+                                WideTile::WideBoxLeft => self.try_move_box(
+                                    &next_position,
+                                    &next_position.neighbor(Direction::Right).unwrap(),
+                                    dir,
+                                ),
+                                WideTile::WideBoxRight => self.try_move_box(
+                                    &next_position.neighbor(Direction::Left).unwrap(),
+                                    &next_position,
+                                    dir,
+                                ),
+                                WideTile::Floor => true,
+                                _ => false,
+                            } {
+                                *self.map.tile_mut(&next_position).unwrap() = cur_tile;
+                                *self.map.tile_mut(&pos).unwrap() = WideTile::Floor;
+                                self.map.robot_pos = next_position;
+                                return true;
+                            }
+                        }
+                    }
+                }
+                WideTile::Floor => return true,
+                _ => (),
+            }
+        }
+
+        false
+    }
+
+    pub fn map(&self) -> &Map<WideTile> {
+        &self.map
+    }
+
+    fn try_move_box(&mut self, left_pos: &Position, right_pos: &Position, dir: Direction) -> bool {
+        debug_assert!(self
+            .map()
+            .tile(left_pos)
+            .is_some_and(|tile| *tile == WideTile::WideBoxLeft));
+        debug_assert!(self
+            .map()
+            .tile(right_pos)
+            .is_some_and(|tile| *tile == WideTile::WideBoxRight));
+
+        match dir {
+            Direction::Left | Direction::Right => {
+                if let Some(next_pos) = if dir == Direction::Left {
+                    left_pos.neighbor(dir)
+                } else {
+                    right_pos.neighbor(dir)
+                } {
+                    if let Some(next_tile) = self.map().tile(&next_pos).copied() {
+                        if match next_tile {
+                            WideTile::WideBoxLeft => self.try_move_box(
+                                &next_pos,
+                                &next_pos.neighbor(Direction::Right).unwrap(),
+                                dir,
+                            ),
+                            WideTile::WideBoxRight => self.try_move_box(
+                                &next_pos.neighbor(Direction::Left).unwrap(),
+                                &next_pos,
+                                dir,
+                            ),
+                            WideTile::Floor => true,
+                            _ => false,
+                        } {
+                            if dir == Direction::Left {
+                                *self.map.tile_mut(&next_pos).unwrap() = WideTile::WideBoxLeft;
+                                *self.map.tile_mut(left_pos).unwrap() = WideTile::WideBoxRight;
+                                *self.map.tile_mut(right_pos).unwrap() = WideTile::Floor;
+                            } else {
+                                *self.map.tile_mut(&next_pos).unwrap() = WideTile::WideBoxRight;
+                                *self.map.tile_mut(right_pos).unwrap() = WideTile::WideBoxLeft;
+                                *self.map.tile_mut(left_pos).unwrap() = WideTile::Floor;
+                            }
+
+                            return true;
+                        }
+                    }
+                }
+            }
+            Direction::Up | Direction::Down => {
+                if let Some(next_left_pos) = left_pos.neighbor(dir) {
+                    if let Some(next_right_pos) = right_pos.neighbor(dir) {
+                        if self.can_move_to(&next_left_pos, dir)
+                            && self.can_move_to(&next_right_pos, dir)
+                        {
+                            self.try_move(&next_left_pos, dir);
+                            self.try_move(&next_right_pos, dir);
+                            *self.map.tile_mut(&next_left_pos).unwrap() = WideTile::WideBoxLeft;
+                            *self.map.tile_mut(&next_right_pos).unwrap() = WideTile::WideBoxRight;
+                            *self.map.tile_mut(left_pos).unwrap() = WideTile::Floor;
+                            *self.map.tile_mut(right_pos).unwrap() = WideTile::Floor;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        false
+    }
+
+    fn can_move_to(&self, pos: &Position, dir: Direction) -> bool {
+        if let Some(tile) = self.map.tile(pos) {
+            match tile {
+                WideTile::WideBoxLeft | WideTile::WideBoxRight => {
+                    if let Some(next_pos) = pos.neighbor(dir) {
+                        if let Some((next_left_pos, next_right_pos)) =
+                            if *tile == WideTile::WideBoxLeft {
+                                pos.neighbor(Direction::Right)
+                                    .and_then(|right_pos| right_pos.neighbor(dir))
+                                    .map(|next_right_pos| (next_pos, next_right_pos))
+                            } else {
+                                pos.neighbor(Direction::Left)
+                                    .and_then(|left_pos| left_pos.neighbor(dir))
+                                    .map(|next_left_pos| (next_left_pos, next_pos))
+                            }
+                        {
+                            return self.can_move_to(&next_left_pos, dir)
+                                && self.can_move_to(&next_right_pos, dir);
+                        }
+                    }
+                }
+                WideTile::Floor => return true,
+                _ => (),
+            }
+        }
+
+        false
+    }
+}
+
+pub struct PlainMapBuilder {
+    tiles: Vec<PlainTile>,
     robot_pos: Option<Position>,
     row_n: usize,
     col_n: Option<usize>,
 }
 
-impl MapBuilder {
+impl PlainMapBuilder {
     pub fn new() -> Self {
         Self {
             tiles: Vec::new(),
@@ -228,8 +462,8 @@ impl MapBuilder {
 
         for (ind, c) in text.chars().enumerate() {
             self.tiles.push(match c {
-                '#' => Tile::Wall,
-                '.' => Tile::Floor,
+                '#' => PlainTile::Wall,
+                '.' => PlainTile::Floor,
                 '@' => {
                     let this_robot_pos = Position::new(self.row_n, ind);
                     if *self.robot_pos.get_or_insert(this_robot_pos.clone()) != this_robot_pos {
@@ -239,9 +473,9 @@ impl MapBuilder {
                         ));
                     }
 
-                    Tile::Robot
+                    PlainTile::Robot
                 }
-                'O' => Tile::Box,
+                'O' => PlainTile::Box,
                 other => return Err(Error::InvalidCharforMap(other)),
             });
         }
@@ -250,9 +484,9 @@ impl MapBuilder {
         Ok(())
     }
 
-    pub fn build(self) -> Result<Map, Error> {
+    pub fn build(self) -> Result<Map<PlainTile>, Error> {
         if let Some(robot_pos) = self.robot_pos {
-            Ok(Map {
+            Ok(Map::<PlainTile> {
                 tiles: self.tiles,
                 robot_pos,
                 row_n: self.row_n,
@@ -264,11 +498,11 @@ impl MapBuilder {
     }
 }
 
-pub fn read_game<P: AsRef<Path>>(path: P) -> Result<(Map, Vec<Direction>)> {
+pub fn read_game<P: AsRef<Path>>(path: P) -> Result<(Map<PlainTile>, Vec<Direction>)> {
     let file = File::open(&path)
         .with_context(|| format!("Failed to open given file({}).", path.as_ref().display()))?;
     let reader = BufReader::new(file);
-    let mut builder = MapBuilder::new();
+    let mut builder = PlainMapBuilder::new();
     let mut enum_lines = reader.lines().enumerate();
     while let Some((ind, line)) = enum_lines.next() {
         let line = line.with_context(|| {
