07e039c00ea752dfbc6c0fba1abe4325817f140f
Leizy <leizy0@users.noreply.github.com>
2024-12-22 19:01:31 +0800

    Sovled part 1 of day 22 in 2024, implemente the secret number as an iterator, it's more clear can get helps from the plenty of methods for Iterator.

---
+++ 2024/day22/src/lib.rs
@@ -0,0 +1,132 @@
+use std::{
+    error,
+    fmt::Display,
+    fs::File,
+    io::{BufRead, BufReader},
+    path::{Path, PathBuf},
+};
+
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidSecretNumberText(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::InvalidSecretNumberText(s) => {
+                write!(f, "Invalid text({}) for secret number.", s)
+            }
+        }
+    }
+}
+
+impl error::Error for Error {}
+
+#[derive(Debug, Parser)]
+pub struct CLIArgs {
+    pub input_path: PathBuf,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct SecretNumber {
+    n: usize,
+}
+
+impl TryFrom<&str> for SecretNumber {
+    type Error = Error;
+
+    fn try_from(value: &str) -> std::result::Result<Self, Self::Error> {
+        Ok(Self {
+            n: value
+                .parse::<usize>()
+                .map_err(|_| Error::InvalidSecretNumberText(value.to_string()))?,
+        })
+    }
+}
+
+impl Iterator for SecretNumber {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.generate())
+    }
+}
+
+impl SecretNumber {
+    pub fn new(n: usize) -> Self {
+        Self { n }
+    }
+
+    pub fn generate(&mut self) -> usize {
+        self.mix(self.n * 64);
+        self.prune();
+
+        self.mix(self.n / 32);
+        self.prune();
+
+        self.mix(self.n * 2048);
+        self.prune();
+
+        self.n
+    }
+
+    fn mix(&mut self, value: usize) {
+        self.n ^= value;
+    }
+
+    fn prune(&mut self) {
+        self.n %= 16777216;
+    }
+}
+
+#[test]
+fn test_generate() {
+    let sec_n = SecretNumber::new(123);
+    let target_seq = vec![
+        15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254,
+    ];
+    let generate_seq = sec_n.take(target_seq.len()).collect::<Vec<_>>();
+    assert!(target_seq == generate_seq);
+}
+
+#[test]
+fn test_mix() {
+    let mut sec_n = SecretNumber::new(42);
+    sec_n.mix(15);
+    assert!(sec_n.n == 37);
+}
+
+#[test]
+fn test_prune() {
+    let mut sec_n = SecretNumber::new(100000000);
+    sec_n.prune();
+    assert!(sec_n.n == 16113920);
+}
+
+pub fn read_init_numbers<P: AsRef<Path>>(path: P) -> Result<Vec<SecretNumber>> {
+    let file = File::open(&path)
+        .with_context(|| format!("Failed to open given file({}).", path.as_ref().display()))?;
+    let reader = BufReader::new(file);
+    reader
+        .lines()
+        .enumerate()
+        .map(|(ind, line)| {
+            line.with_context(|| {
+                format!(
+                    "Failed to read line {} in given file({}).",
+                    ind + 1,
+                    path.as_ref().display()
+                )
+            })
+            .and_then(|s| {
+                SecretNumber::try_from(s.as_str()).with_context(|| {
+                    format!("Failed to parse secret number from given line({}).", s)
+                })
+            })
+        })
+        .collect()
+}
---
+++ 2024/day22/tests/part1.rs
@@ -0,0 +1,10 @@
+use assert_cmd::Command;
+use predicates::prelude::predicate::str;
+
+#[test]
+fn part1_output_right_answer() {
+    let mut cmd = Command::cargo_bin("part1").unwrap();
+    cmd.arg("inputs.txt");
+
+    cmd.assert().success().stdout(str::contains("17577894908"));
+}
