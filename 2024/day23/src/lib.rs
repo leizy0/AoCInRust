bfb82417e29856798230e876c69e1f83a0919661
Leizy <leizy0@users.noreply.github.com>
2024-12-23 15:23:58 +0800

    Solved part 2 of day 23 in 2024, build the group from the bottom up, one loop one bigger.

--- 2024/day23/src/lib.rs
+++ 2024/day23/src/lib.rs
@@ -93,6 +93,41 @@ impl ComputerLinkGraph {
 
         groups
     }
+
+    pub fn max_all_connected_groups(&self) -> HashSet<Vec<Computer>> {
+        let mut groups = HashSet::new();
+        for (computer, neighbors) in self.link_map.iter() {
+            for neighbor in neighbors {
+                let mut link_group = Vec::from([computer.clone(), neighbor.clone()]);
+                link_group.sort_unstable();
+                groups.insert(link_group);
+            }
+        }
+
+        loop {
+            let mut next_groups = HashSet::new();
+            for group in groups.iter() {
+                for (computer, neighbors) in self.link_map.iter() {
+                    if !group.binary_search(computer).is_ok()
+                        && group.iter().all(|member| neighbors.contains(member))
+                    {
+                        let mut new_group = group.clone();
+                        new_group.push(computer.clone());
+                        new_group.sort_unstable();
+                        next_groups.insert(new_group);
+                    }
+                }
+            }
+
+            if next_groups.len() > 0 {
+                groups = next_groups;
+            } else {
+                break;
+            }
+        }
+
+        groups
+    }
 }
 
 pub fn read_links<P: AsRef<Path>>(path: P) -> Result<Vec<(Computer, Computer)>> {
